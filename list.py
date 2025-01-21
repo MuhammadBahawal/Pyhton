3b4f3a1a6143d7826f2e6628e95c620320407412
Muhammad Bahawal <167578686+MuhammadBahawal@users.noreply.github.com>
2025-01-21 13:00:12 +0500

    list

---
+++ list.py
@@ -0,0 +1,13 @@
+# list is a collection of data it is similar to array 
+
+data = [1,2,3,4,5,6,7,"hello"]
+print(data)
+
+print(data[0])
+print(data[2:6]) #required limited list from list 
+print(data[2:]) #start from index 2 and print at end
+print(data[:5]) # start from 0 index and end required 
+
+#cange list items 
+data[2] = "Ahmad"
+print(data)
