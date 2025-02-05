5518e69a8f5a8f26d82314fc4315e0c48dbf8c4e
Muhammad Bahawal <167578686+MuhammadBahawal@users.noreply.github.com>
2025-02-05 11:15:29 +0500

    Added tempCodeRunnerFile.py

---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,14 @@
+num = [1, 2, 3, 4, 5, 6, 1, 2, 3, 4, 5]
+# uniques_num = []
+# for i in num:
+#     if i not in uniques_num:
+#         uniques_num.append(i)
+# print(uniques_num)
+
+# # 2nd method 
+
+# unique = list(dict.fromkeys(num))
+# print(unique)
+# # 3rd method
+# unique1= list(set(num))
+# print(unique1)
