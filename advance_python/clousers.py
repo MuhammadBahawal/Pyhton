625af8adf80a858432d200093b6c88925856bafc
MuhammadBahawal <muhammadbahawalsher@gmail.com>
2025-02-11 21:30:50 +0500

    advance python clouser and decorator

---
+++ advance_python/clousers.py
@@ -0,0 +1,13 @@
+#A closure is a function that remembers variables from
+# its enclosing scope even after the scope has finished executing. 
+
+def outer_function(x):
+    def inner_function():
+        print(f"Value of x is: {x}")  # Inner function remembers 'x'
+    return inner_function  # Returning the inner function
+
+# Creating a closure
+closure_func = outer_function(10)
+
+# Calling the closure function
+closure_func()  # Output: Value of x is: 10
---
+++ advance_python/decorators.py
@@ -0,0 +1,15 @@
+# A decorator is a function that modifies another
+# function without changing its code. It uses the @decorator_name syntax.
+
+def my_decorator(func):
+    def wrapper():
+        print("Before function call")
+        func()
+        print("After function call")
+    return wrapper
+
+@my_decorator
+def say_hello():
+    print("Hello!")
+
+say_hello()
